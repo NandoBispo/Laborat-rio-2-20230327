143f9356d56bf0d44a19c55900f978ca4ccca87c
Fernando Bispo <86989504+NandoBispo@users.noreply.github.com>
2023-03-27 05:49:06 -0300

    construção 1

--- lab02.R
+++ lab02.R
@@ -10,5 +10,56 @@ pacman::p_load(tidyverse, janitor, kableExtra, summarytools,
 # https://curso-r.githud.io/zen-do-r/git-githud.html
 gitcreds::gitcreds_set()
 usethis::use_git()
-usethis::use_githud()
+usethis::use_github()
 # _________________________________________________
+
+# DADOS 1 ----
+dados <- read.csv2("possum_red.csv")
+
+glimpse(dados)
+
+dados <- dados |> 
+  mutate(
+    skullw = as.numeric(skullw),
+    totlngth = as.numeric(totlngth),
+    sex = forcats::as_factor(sex)
+  )
+
+glimpse(dados)
+
+
+# DADOS 2 ----
+dados2 <- read.csv2("florida.csv")
+
+glimpse(dados2)
+
+# AED ----
+dados|>
+  summarytools::descr(
+      stats = c("min", "q1", "med", "mean","q3", "max",  "sd", "cv"),
+      # round.digits = 3,
+      justify = "c",
+      style = "grid", #' rmarkdown',
+      transpose = T
+    ) |>
+    # round(., 2) %>%
+    kdl(
+      caption = "Tabela 1: Medidas Resumo",
+      digits = 2,
+      format.args=list(big.mark=".", decimal.mark=","),
+      align = "c", 
+      row.names = T,
+      col.names =
+        c("Min", "Q1", "Med", "Média", "Q3", "Max", "Desvio Padrão", "CV")
+    )|>
+    kable_material(c("striped", "hover", "condensed"))|>
+    # kadle_styling(
+    #   # dootstrap_options = c("striped", "hover", "condensed", "responsive"),
+    #   dootstrap_options = c("striped", "hover"),
+    #   full_width = F,
+    #   fixed_thead = T # Fixa o cadeçalho ao rolar a tadela.
+    # ) %>%
+    footnote(general = "Fonte: Instituto Nacional de Diabetes e de Doenças Digestivas e Renais - EUA") |>
+    kable_material()
+  # add_header_adove(c("Características", "Medidas de Tendência Central e Variadilidade" = 8))
+
